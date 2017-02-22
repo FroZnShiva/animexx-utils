fc34a4f901d27079261631e94d0b121d09b9be6f
FroZnShiva <froznshiva@gmx.net>
2017-02-22 20:44:46 +0100

    Enforce the usage of TLSv1.2

--- animexx.py
+++ animexx.py
@@ -1,7 +1,30 @@
 import requests
+import ssl
+
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.poolmanager import PoolManager
 
 Host = 'ssl.animexx.de'
 CertificateFile = './certificates/*.animexx.de'
+
+class HttpsAdapter(HTTPAdapter):
+	def init_poolmanager(self, connections, maxsize, block=False):
+		self.poolmanager = PoolManager(
+			num_pools = connections,
+			maxsize = maxsize,
+			block = block,
+			ssl_version = ssl.PROTOCOL_TLSv1_2,
+		)
+
+def new_session():
+	session = requests.Session()
+	session.mount('https://', HttpsAdapter())
+	return session
+
+def fetch(url, cookies):
+	session = new_session()
+	response = session.get(url, cookies = cookies, verify = CertificateFile)
+	return response
 
 def bake_cookies(sessionId, autologin):
 	cookies = requests.cookies.RequestsCookieJar()
